a8d2e197ef4025f0ccf869bdc83332bf414fd1ea
Steven Gong <gonghaima@hotmail.com>
2024-06-14 14:02:47 +1000

    HeaderBox component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,22 @@
+import HeaderBox from '@/components/HeaderBox';
 import React from 'react';
 
 function Home() {
-  return <div>Home</div>;
+  const loggedIn = { firstName: 'Adrian' };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext = "Access and manage your account and transactions efficiently."
+          />
+        </header>
+      </div>
+    </section>
+  );
 }
 
 export default Home;
