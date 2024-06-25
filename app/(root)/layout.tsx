e74c1d07809494426dcc048ad67e6c746b9f3652
Steven Gong <gonghaima@hotmail.com>
2024-06-25 13:50:58 +1000

    mobile nav

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
+import MobileNav from '@/components/MobileNav';
 import Sidebar from '@/components/Sidebar';
+import Image from 'next/image';
 
 export default function RootLayout({
   children,
@@ -25,6 +27,12 @@ export default function RootLayout({
   return (
     <main className="flex h-screen w-full font-inter">
       <Sidebar user={user} />
+      <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav user={user} />
+          </div>
+        </div>
       {children}
     </main>
   );
