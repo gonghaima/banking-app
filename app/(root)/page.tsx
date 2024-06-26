79a76ec633c0053b3d38e4b18db52d251f1f7f4a
Steven Gong <gonghaima@hotmail.com>
2024-06-26 13:18:05 +1000

    Right side bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,58 @@
 import HeaderBox from '@/components/HeaderBox';
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react';
 
 function Home() {
   const loggedIn = { firstName: 'Adrian' };
+  const account = {
+    transactions: [
+      {
+        id: 1,
+        amount: 100,
+        category: 'Food',
+        date: '2021-10-10',
+        description: 'Bought some food',
+        type: 'expense',
+      },
+      {
+        id: 2,
+        amount: 200,
+        category: 'Salary',
+        date: '2021-10-10',
+        description: 'Got paid',
+        type: 'income',
+      },
+      {
+        id: 3,
+        amount: 300,
+        category: 'Transport',
+        date: '2021-10-10',
+        description: 'Took a cab',
+        type: 'expense',
+      },
+    ],
+  };
+  const accountsData = [
+    {
+      id: 1,
+      name: 'Bank of America',
+      balance: 1250.35,
+      type: 'savings',
+    },
+    {
+      id: 2,
+      name: 'Chase Bank',
+      balance: 1250.35,
+      type: 'checking',
+    },
+    {
+      id: 3,
+      name: 'Wells Fargo',
+      balance: 1250.35,
+      type: 'savings',
+    },
+  ];
   return (
     <section className="home">
       <div className="home-content">
@@ -21,6 +70,11 @@ function Home() {
           />
         </header>
       </div>
+      <RightSidebar 
+        user={loggedIn}
+        transactions={account?.transactions}
+        banks={accountsData?.slice(0, 2)}
+      />
     </section>
   );
 }
