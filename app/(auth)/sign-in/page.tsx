337f628d275002cd2197b0e4663141ef1534793e
Steven Gong <gonghaima@hotmail.com>
2024-06-29 14:44:06 +1000

    signIn form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+// import AuthForm from '@/components/AuthForm'
 
-function SignIn() {
-  return <div>SignIn</div>;
+import AuthForm from '@/components/AuthForm'
+
+const SignIn = () => {
+  return (
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-in" />
+    </section>
+  )
 }
 
-export default SignIn;
+export default SignIn
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import MobileNav from '@/components/MobileNav';
 import Sidebar from '@/components/Sidebar';
 import Image from 'next/image';
+import { redirect } from "next/navigation";
 
 export default function RootLayout({
   children,
@@ -24,15 +25,19 @@ export default function RootLayout({
     ssn: '123-45-6789',
   };
 
+  const loggedIn = false;
+
+  if (!loggedIn) redirect('/sign-in');
+
   return (
     <main className="flex h-screen w-full font-inter">
       <Sidebar user={user} />
       <div className="root-layout">
-          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
-          <div>
-            <MobileNav user={user} />
-          </div>
+        <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+        <div>
+          <MobileNav user={user} />
         </div>
+      </div>
       {children}
     </main>
   );
